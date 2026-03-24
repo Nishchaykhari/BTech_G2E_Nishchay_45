6cb4e48d5f4d038beca980d41f16a4e477adb599
Nishchaykhari <dpsvn.nish17145@gmail.com>
2026-03-24 14:32:17 +0530

    Create program5.c

---
+++ program5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+   int a, b, temp;
+   printf("Enter any 2 numbers: ");
+   scanf("%d %d", &a, &b);
+   printf("Before swapping a=%d, b=%d ", a, b);
+    
+  temp = a;
+   a = b;
+   b = temp;
+   printf("After swapping =%d, b=%d", a, b);
+   return 0; 
+}
